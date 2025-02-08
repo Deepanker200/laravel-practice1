5c891903ba8efbe1e3fa8921b99ffeafc77b8aad
Deepanker Tiwari <deepamamazing@gmail.com>
2025-02-08 23:48:17 +0530

    Route group with Controller

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -6,5 +6,23 @@ use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
-    //
+    public function show()
+    {
+        return "list of students";
+    }
+
+    public function add()
+    {
+        return "student added";
+    }
+
+    public function delete()
+    {
+        return "Deleted Students";
+    }
+
+    public function about($name)
+    {
+        return $name;
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\StudentController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -19,15 +20,27 @@ Route::get('/about', function () {
 //     return view('about', compact('name')); 
 // });
 
-Route::get('adduser',[UserController::class,'view']);
-Route::post('adduser',[UserController::class,'index']);
+Route::get('adduser', [UserController::class, 'view']);
+Route::post('adduser', [UserController::class, 'index']);
 
-Route::view('url','url');
+Route::view('url', 'url');
 
-Route::prefix('student')->group(function(){
-    Route::view('/home','home2');
-    Route::get('/show',[HomeController::class,'show']);
-    Route::get('/add',[HomeController::class,'add']);
+//Route with Prefix
+
+Route::prefix('student')->group(function () {
+    Route::view('/home', 'home2');
+    Route::get('/show', [HomeController::class, 'show']);
+    Route::get('/add', [HomeController::class, 'add']);
 });
 
 //Note: Double prefix can be used as well
+
+
+//Route with Controller
+
+Route::controller(StudentController::class)->group(function () {
+    Route::get('show', 'show');
+    Route::get('add',  'add');
+    Route::get('delete', 'delete');
+    Route::get('about/{name}', 'about');
+});
